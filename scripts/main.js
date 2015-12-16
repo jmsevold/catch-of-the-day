35b0781d4ed64b33f478ed9c7b9dffc19d13b797
Jonathan Sevold <jmsevold@gmail.com>
2015-12-16 12:27:54 -0800

    Helper Functions in React

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var Router = ReactRouter.Router;
 var Route  = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
+var helpers = require('./helpers.js');
 
 
 var App = React.createClass({
@@ -68,7 +69,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" required/>
+        <input type="text" ref="storeId" defaultValue={helpers.getFunName()}required/>
         <input type="Submit"/>
       </form>
     )
