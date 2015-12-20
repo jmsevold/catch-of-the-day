6e51437f6522b35b95e2c44c9763540640a460d9
Jonathan Sevold <jmsevold@gmail.com>
2015-12-20 15:26:11 -0800

    Displaying our Order State with JSX

--- scripts/main.js
+++ scripts/main.js
@@ -48,7 +48,7 @@ var App = React.createClass({
             {Object.keys(fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order />
+        <Order orders={this.state.orders} fishes={this.state.fishes} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
@@ -144,9 +144,43 @@ var Header = React.createClass({
 });
 
 var Order = React.createClass({
+  renderOrder : function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.orders[key];
+
+    if (!fish) {
+      return <li key={key}>Sorry, fish no longer available!</li>
+    }
+    return (
+      <li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{helpers.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
   render: function() {
+    var orderIds = Object.keys(this.props.orders);
+    var total         = orderIds.reduce((prevTotal,key)=> {
+      var fish        = this.props.fishes[key]
+      var count       = this.props.orders[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if (fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0)
+      }
+      return prevTotal;
+    },0);
     return(
-      <p>Order</p>
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total: {helpers.formatPrice(total)}</strong>
+          </li>
+        </ul>
+      </div>
     )
   }
 });
